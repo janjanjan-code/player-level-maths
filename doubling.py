0370726aa1f2fa86bbc7dea0ed07d218ed62b847
janjanjan-code <61359066+janjanjan-code@users.noreply.github.com>
2020-02-22 18:03:35 +0000

    Create doubling.py

---
+++ doubling.py
@@ -0,0 +1,17 @@
+import math
+
+database = [
+{"name": "player1", "wins": 5},
+{"name": "player2", "wins": 15},
+{"name": "player3", "wins": 25}
+]
+
+for l in range(20):
+    w = pow(2,l)
+    print("level {} needs {} wins".format(l,w))
+
+# get wins for a user and calculate level
+for player in database:
+    pl = math.floor(math.log(player['wins']) / math.log(2)) #round the level down to the nearest int
+    wr = math.pow(2,(pl + 1)) - player['wins']
+    print("{}, you are level {} and need {} wins to get to the next level".format(player['name'], pl, wr))
